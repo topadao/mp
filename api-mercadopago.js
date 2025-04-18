e78aa7f10dabcefdacc5717bbb66857e088e4738
topadao <topadaoinside@gmail.com>
2025-04-18 16:49:22 -0300

    Update api-mercadopago.js

--- api-mercadopago.js
+++ api-mercadopago.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT ?? 10000;
 
 app.use(cors());
 app.use(express.json());
